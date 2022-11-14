914ee135dd999bef63800fd42829abd13b4932c8
Roy Sivan <roy@roysivan.com>
2022-11-13 22:59:41 -0500

    initial commit iwth blockStylesToCSS

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// Types
+interface blockStylesToCSSProps {
+  [key: string]: any;
+}
+
+export const blockStylesToCSS = ( attributes: blockStylesToCSSProps ) => {
+  console.log('here');
+
+  return attributes;
+}
